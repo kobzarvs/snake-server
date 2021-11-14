7dc08bed3ada5f9b6b444b4041729252a221c359
kobzarvs <kobzarvs@gmail.com>
2021-11-14 04:19:32 +0300

    heroku

--- src/server.js
+++ src/server.js
@@ -1,21 +1,54 @@
+// const http = require('http');
+// const socketio = require('socket.io');
 const express = require('express');
 const {ExpressPeerServer} = require('peer');
-const app = express();
 
+const PORT = process.env.PORT;
+
+const app = express();
 app.enable('trust proxy');
 
-const PORT = process.env.PORT || 9000;
+// const server = http.Server(app);
+// const io = socketio(server);
+// io.sockets.on('connection', (socket) => {
+//   console.log(socket);
+// });
+
+const clients = new Map();
+
+app.use(express.static('public'));
+
 const server = app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+  console.log(`App listening on port http://localhost:${PORT}`);
   console.log('Press Ctrl+C to quit.');
 });
 
+module.exports = app;
+
 const peerServer = ExpressPeerServer(server, {
-  path: '/',
   allow_discovery: true,
-  key: 'snake',
 });
 
-app.use('/', peerServer);
+peerServer
+  .on('connection', (client) => {
+    console.log('connectin', client.getId());
+    for(let c of clients.values()) {
+      c.send({type: 'SNAKE/ADD_PEER', payload: client.getId()});
+    }
+    clients.set(client.getId(), client);
+  })
+  .on('disconnect', (client) => {
+    console.log('disconnect', client.getId());
+    clients.delete(client.getId());
+    for(let c of clients.values()) {
+      c.send({type: 'SNAKE/REMOVE_PEER', payload: client.getId()});
+    }
+  })
+  .on('message', (client, message) => {
+    console.log('message', message);
+  })
+  .on('error', (error) => {
+    console.error('error', error);
+  })
 
-module.exports = app;
+app.use(peerServer);
